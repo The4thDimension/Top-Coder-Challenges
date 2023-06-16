4e44e5ec376f0153ee1e51ac195e0545b6afe627
Mahesh <pmaheshiyer@gmail.com>
2023-06-16 18:11:02 +0530

    Grid Movement Basic

---
+++ HappyGrid.py
@@ -0,0 +1,36 @@
+import sys
+
+N = int(input())
+C = int(input())
+K = int(input())
+P = int(input())
+
+# read grid
+grid = [[-1 for x in range(N)] for y in range(N)]
+for r in range(N):
+    for c in range(N):
+        grid[r][c] = int(input())
+
+i = 0
+moves = 0
+numMoves = 20
+N2 = (N-1)*(N-1)
+print(numMoves)
+while moves < numMoves:
+
+    num = (i*8009+104729) % N2
+    r = num//(N-1)
+    c = num % (N-1)
+    i += 1
+
+    if grid[r][c] == 0:
+        continue
+
+    if i % 2 == 1 and grid[r][c+1] != 0:
+        print(r, c, r, c+1)
+        moves += 1
+    elif i % 2 == 0 and grid[r+1][c] != 0:
+        print(r, c, r+1, c)
+        moves += 1
+
+sys.stdout.flush()
